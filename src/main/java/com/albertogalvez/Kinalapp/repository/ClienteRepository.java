621e67e39bd8a526a5a084096c38963a2a3918f3
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-03-24 09:33:31 -0600

    feat: Agrega la primer fase del proyecto KinalApp funcionando

---
+++ src/main/java/com/albertogalvez/Kinalapp/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.albertogalvez.Kinalapp.repository;
+
+import com.albertogalvez.Kinalapp.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente,String> {
+    List<Cliente> findByEstado(int estado);
+}
+

32d64923ecd52021c5cc082e1f9f716a6a4aeca7
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-03-24 22:46:53 -0600

    feat(usuario): crear repositorio UsuarioRepository

---
+++ src/main/java/com/albertogalvez/Kinalapp/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.albertogalvez.Kinalapp.repository;
+
+import com.albertogalvez.Kinalapp.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    List<Usuario> findByEstado(int estado);
+    Optional<Usuario> findByUsername(String username);
+}

e61dd870805487eceefd779667fbcf77250cd2e9
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-03-25 07:38:00 -0600

    refactor: agrega comentarios para especificar la funcionalidad de los métodos findByCliente_DPICliente y findByEstado.

--- src/main/java/com/albertogalvez/Kinalapp/repository/VentasRepository.java
+++ src/main/java/com/albertogalvez/Kinalapp/repository/VentasRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VentasRepository extends JpaRepository<Ventas, Integer> {
+    //Buscar por estado
     List<Ventas> findByEstado(int estado);
-    // Puedes agregar otros métodos de búsqueda, por ejemplo por cliente
+    //Buscar por DPI
     List<Ventas> findByCliente_DPICliente(String dpi);
 }

cae69e6c23f5c97dcd9a86e1167be7fbaba227d7
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-03-28 00:39:21 -0600

    Refactor: se eliminan comentaríos de indicacion.

--- src/main/java/com/albertogalvez/Kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/albertogalvez/Kinalapp/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package com.albertogalvez.Kinalapp.repository;
 
 import com.albertogalvez.Kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 

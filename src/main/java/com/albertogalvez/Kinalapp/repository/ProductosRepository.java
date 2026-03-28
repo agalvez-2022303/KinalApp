8adbb347239090490414c03c2cfa2c6c8639302b
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-03-28 00:31:20 -0600

    fix: cambia int a long.

--- src/main/java/com/albertogalvez/Kinalapp/repository/ProductosRepository.java
+++ src/main/java/com/albertogalvez/Kinalapp/repository/ProductosRepository.java
@@ -4,6 +4,6 @@ import com.albertogalvez.Kinalapp.entity.Productos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface ProductosRepository extends JpaRepository<Productos, Long> { // Long
+public interface ProductosRepository extends JpaRepository<Productos, Long> {
     List<Productos> findByEstado(int estado);
 }

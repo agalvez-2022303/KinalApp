cf6f409b3d8d2232871daed5572e33ba726df093
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-03-28 00:19:36 -0600

    fix: se realiza cambio de tipos primitivo a clase envolvente.

--- src/main/java/com/albertogalvez/Kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/albertogalvez/Kinalapp/repository/DetalleVentaRepository.java
@@ -2,7 +2,6 @@ package com.albertogalvez.Kinalapp.repository;
 
 import com.albertogalvez.Kinalapp.entity.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {

59dfee3e4f5b505bf85fd9dd31b3b596804fdc59
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-04-21 16:13:19 -0600

    fix: arregla errores

--- src/main/java/com/albertogalvez/Kinalapp/service/ClienteService.java
+++ src/main/java/com/albertogalvez/Kinalapp/service/ClienteService.java
@@ -8,64 +8,105 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+//Anotacion que registra un bean como un Bean de Spring
+//Que la clase contiene la logica del negocio
 @Service
-@Transactional
-public class ClienteService implements IClienteService {
 
+//Por defecto todos los metodos de esta clase seran transccionales
+//Esto es que puede o no ocurrir algo.
+@Transactional
+
+public class ClienteService implements IClienteServise{
+
+    /* private solo accesible solo en la base
+     * ClienteRepositort: es el repositorio para acceder a la base de datos
+     * Inyeccion de dependencias Spring nos da el repositorio
+     */
     private final ClienteRepository clienteRepository;
+
+    /*
+     * Constructor: Este se ejecuta al crear el objeto
+     * Parametros: Spring pasa el repositorio automaticamente y a esto se le conoce como inyeccion de dependencias
+     * Asignamos: el repositorio a nuestra variable de clase.
+     */
 
     public ClienteService(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
     }
 
+    /*
+     * @Override: Indicamos que estamos implementando un metodo de la interfaz
+     */
     @Override
-    @Transactional(readOnly = true)
-    public List<Cliente> listarEstado(int estado) {
-        return clienteRepository.findByEstado(estado);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public boolean existePorDPI(String dpi) {
-        return clienteRepository.existsById(dpi);
-    }
-
-    @Override
+    /*
+     * reafOnlu = true: optimiza la consulta y no bloquea la base de datos
+     */
     @Transactional(readOnly = true)
     public List<Cliente> listarTodos() {
         return clienteRepository.findAll();
+        /*
+         * Llama al metodo findAll() del repositorio de Spring Data JPA
+         * Este metodo hace exactamente el selct * from clientes
+         */
+    }
+
+    //
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> listarEstadosActivos() {
+        return clienteRepository.findAll()
+                .stream() //se usa para pasar una coleccion de datos en una secuencia para que sea más facil el filtrar
+                //datos
+                .filter(cliente -> cliente.getEstado() == 1) //este filtra la secuencia de datos que nos da el Stream
+                //para que si el cliente esta en activo pase y si no pues se quede afuera del JSON
+                .toList();// Convierte la secuencia de datos en una lista nuevamente.
     }
 
     @Override
     public Cliente guardar(Cliente cliente) {
+        /*
+         * Metodo de Guardar crea un cliente
+         * Aca es donde colocamos la logica del negocio antes de guardar
+         * Primer: Validamos el dato
+         * */
         validarCliente(cliente);
-        if (cliente.getEstado() == 0) {
+        if (cliente.getEstado() == 0)
             cliente.setEstado(1);
-        }
+
         return clienteRepository.save(cliente);
     }
 
     @Override
     @Transactional(readOnly = true)
-    public Optional<Cliente> bucarPorDPI(String dpi) {
+    //Optional: nos evita el NullPointerException
+    public Optional<Cliente> buscarPorDPI(String dpi) {
+        //Busca un cliente por DPI
         return clienteRepository.findById(dpi);
     }
 
     @Override
-    public Cliente actualizar(String dpi, Cliente clienteActualizado) {
-        Cliente existente = clienteRepository.findById(dpi)
-                .orElseThrow(() -> new RuntimeException("Cliente no encontrado con DPI: " + dpi));
-        existente.setNombreCliente(clienteActualizado.getNombreCliente());
-        existente.setApellidoCliente(clienteActualizado.getApellidoCliente());
-        existente.setDireccion(clienteActualizado.getDireccion());
-        validarCliente(existente);
-        return clienteRepository.save(existente);
+    public Cliente actualizar(String dpi, Cliente cliente) {
+        /*
+         * Actualiza un cliente existente
+         * */
+        if(!clienteRepository.existsById(dpi)){
+            throw new RuntimeException("Cliente no encontrado con DPI" + dpi);
+            //Si no existe se lanza una excepcion (error controlado)
+        }
+        /*
+         * 1. Se asegura que el DPI del objeto coincida con el de la url
+         * 2. Por seguridad se usa el DPI de la URL y no el de JSON
+         * */
+        cliente.setDPICliente(dpi);
+        validarCliente(cliente);
+        return clienteRepository.save(cliente);
     }
 
     @Override
     public void eliminar(String dpi) {
-        if (!clienteRepository.existsById(dpi)) {
-            throw new RuntimeException("Cliente no encontrado con el DPI: " + dpi);
+        //Eliminar un cliente por DPI
+        if(!clienteRepository.existsById(dpi)){
+            throw new RuntimeException("El cliente no se encontro con el DPI" + dpi);
         }
         clienteRepository.deleteById(dpi);
     }
@@ -73,18 +114,29 @@ public class ClienteService implements IClienteService {
     @Override
     @Transactional(readOnly = true)
     public boolean existeDPI(String dpi) {
+        //Verifica si el cliente existe
         return clienteRepository.existsById(dpi);
+        // retorna true o false w
     }
 
-    private void validarCliente(Cliente cliente) {
-        if (cliente.getDpiCliente() == null || cliente.getDpiCliente().trim().isEmpty()) {
+    //Metodo privado(solo puede utilizarse dentro de la clase)
+    private void validarCliente(Cliente cliente){
+        /*
+         * Validaciones: este metodo se hara privado por que es algo interno del servicio
+         */
+        if(cliente.getDPICliente() == null  || cliente.getDPICliente().trim().isEmpty()){
+            //si el DPI es null o esta vacio despues de quitar espacios lanza una exception con un mensaje
             throw new IllegalArgumentException("El DPI es un dato obligatorio");
         }
-        if (cliente.getNombreCliente() == null || cliente.getNombreCliente().trim().isEmpty()) {
+
+        if(cliente.getNombreCliente() == null || cliente.getNombreCliente().trim().isEmpty()){
             throw new IllegalArgumentException("El nombre es un dato obligatorio");
         }
-        if (cliente.getApellidoCliente() == null || cliente.getApellidoCliente().trim().isEmpty()) {
-            throw new IllegalArgumentException("El apellido es un dato obligatorio");
+
+        if (cliente.getApellidoCliente() == null || cliente.getApellidoCliente().trim().isEmpty()){
+            throw new IllegalArgumentException("El Apellido es un dato obligatorio");
         }
+
     }
+
 }

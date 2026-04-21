eab0cb1e72bdf2f8d75637717e88fa0556af0644
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-04-21 16:11:05 -0600

    feat: se agrega interfaz grafica

---
+++ src/main/java/com/albertogalvez/Kinalapp/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.albertogalvez.Kinalapp.config;
+
+import com.albertogalvez.Kinalapp.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/login", "/register",
+                        "/error",          // ← AGREGAR ESTO
+                        "/css/**", "/js/**", "/images/**",
+                        "/test"
+                );
+    }
+}

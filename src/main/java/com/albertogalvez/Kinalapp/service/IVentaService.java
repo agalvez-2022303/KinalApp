0e0e4d9ce8360f28bbe6edc8d938544d0f012729
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-04-21 16:24:00 -0600

    fix: corrige errores

--- src/main/java/com/albertogalvez/Kinalapp/service/IVentaService.java
+++ src/main/java/com/albertogalvez/Kinalapp/service/IVentaService.java
@@ -1,16 +1,21 @@
 package com.albertogalvez.Kinalapp.service;
 
 import com.albertogalvez.Kinalapp.entity.Venta;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface IVentaService {
+
     List<Venta> listarTodos();
-    Optional<Venta> buscarPorCodigo(Long codigo);
-    Venta guardar(Venta venta);
-    Venta actualizar(Long codigo, Venta venta);
-    void eliminar(Long codigo);
-    boolean existePorCodigo(Long codigo);
-    List<Venta> listarPorEstado(int estado);
-    List<Venta> listarPorCliente(String dpiCliente);
+
+    List<Venta> listarEstadoVenta();
+
+    Venta guardar (Venta venta);
+
+    Optional<Venta> buscarPorId(int id);
+
+    Venta actualizar (int id, Venta venta);
+
+    boolean existePorId(int id);
 }

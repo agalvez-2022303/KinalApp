1f022b72c1e4b9deebb65f56ed56db3ceb8b0e33
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-04-21 16:34:00 -0600

    fix: arregla errores

--- src/main/java/com/albertogalvez/Kinalapp/repository/VentaRepository.java
+++ src/main/java/com/albertogalvez/Kinalapp/repository/VentaRepository.java
@@ -2,9 +2,8 @@ package com.albertogalvez.Kinalapp.repository;
 
 import com.albertogalvez.Kinalapp.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
 public interface VentaRepository extends JpaRepository<Venta, Long> {
-    List<Venta> findByEstado(int estado);
-    List<Venta> findByClienteDpiCliente(String dpiCliente); // ← antes era findByClienteDPICliente
 }

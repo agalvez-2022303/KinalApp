d0891895d9c20798c9075342cf14217a73ce31a8
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-05-01 23:53:30 -0600

    fix: corrige bug logico

--- src/main/java/com/albertogalvez/Kinalapp/config/WebConfig.java
+++ src/main/java/com/albertogalvez/Kinalapp/config/WebConfig.java
@@ -1,26 +1,9 @@
 package com.albertogalvez.Kinalapp.config;
 
-import com.albertogalvez.Kinalapp.interceptor.LoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// Spring Security reemplaza al LoginInterceptor manual
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Autowired
-    private LoginInterceptor loginInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/login", "/register",
-                        "/error",          // ← AGREGAR ESTO
-                        "/css/**", "/js/**", "/images/**",
-                        "/test"
-                );
-    }
 }

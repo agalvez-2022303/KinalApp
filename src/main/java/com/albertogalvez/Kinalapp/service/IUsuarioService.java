cdf5b2e9882e932195361dc250d69e833c0cb1a0
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-05-01 23:50:33 -0600

    fix: se arregla bug logico

--- src/main/java/com/albertogalvez/Kinalapp/service/IUsuarioService.java
+++ src/main/java/com/albertogalvez/Kinalapp/service/IUsuarioService.java
@@ -1,25 +1,17 @@
 package com.albertogalvez.Kinalapp.service;
 
 import com.albertogalvez.Kinalapp.entity.Usuario;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface IUsuarioService{
-
+public interface IUsuarioService {
     List<Usuario> listarTodos();
-
     List<Usuario> listarEstadoUsuario();
-
-    Usuario guardar(Usuario usuario);
-
+    Usuario guardar(Usuario usuario);           // encripta password
+    Usuario guardarSinEncriptar(Usuario usuario); // ya viene encriptado
     Optional<Usuario> buscarPorId(int id);
-
     Optional<Usuario> buscarPorEmail(String email);
-
     Usuario actualizar(int id, Usuario usuario);
-
     void eliminar(int id);
-
     boolean existePorId(int id);
 }
